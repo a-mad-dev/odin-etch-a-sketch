e9f8a59f89cf897d32e7baf0e0ddf6eec2e767e6
muhammedaadhilv69-crypto <muhammedaadhilv69@gmail.com>
2026-06-01 11:57:44 +0300

    v1.0.0.1 - Add drawing vie mouseover detection + proper flex alignment using grid. Format all files with prettier

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".reset");
-
+const boxes = document.querySelectorAll(".box");
 resetBtn.addEventListener("click", resetGrid);
-
 
 createGrid(16);
 
 function createGrid(size) {
-    for (let i = 0; i < size * size; i++) {
-        console.log("creating square");
-        const box = document.createElement("div");
-        box.classList.add("box");
-        container.appendChild(box);
-    }
+  if (isNaN(size) || size < 1) size = 16;
+  if (size > 100) size = 100;
+  container.innerHTML = "";
+  const containerSize = container.clientWidth;
+  const boxSize = containerSize / size;
+  for (let i = 0; i < size * size; i++) {
+    console.log("creating square");
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.classList.add("box");
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "white";
+    });
+    container.appendChild(box);
+  }
 }
 
 function resetGrid() {
-    const size = parseInt(prompt("Size for div?"));
-    createGrid(size);
+  const size = parseInt(prompt("Size for div?"));
+  createGrid(size);
 }
